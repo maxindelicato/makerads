2776b430e051795a9165e808739a8832f2fac565
Danielle Johnson <dinkydani@gmail.com>
2019-03-05 12:29:08 -0500

    fix(meta): image url missing slash

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,7 +23,7 @@ function SEO({
         const metaDescription =
           description || data.site.siteMetadata.description;
         const { author, baseUrl, siteName } = data.site.siteMetadata;
-        const metaImage = image || defaultImage;
+        const metaImage = image ? `/${image}` : defaultImage;
         return (
           <Helmet>
             <meta charSet="utf-8" />
